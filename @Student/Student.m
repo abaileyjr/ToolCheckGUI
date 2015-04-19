4b9fb475bd29c8c66d35689b3f8c1a9d714048c0
Alice <achow2020@berkeley.edu>
2015-04-19 15:10:26 -0700

    added base files for person plus sub classes

---
+++ @Student/Student.m
@@ -0,0 +1,12 @@
+classdef Student < Person
+    %STUDENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
